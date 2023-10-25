9b18f746c038c3d77f9ebd7078a2562c0c184e90
Каменева Наталья <kameneva.natalka@gmail.com>
2023-10-25 15:27:47 +0700

    Исправление ошибок

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-    return """
+    return redirect("/menu", code=302)
+@app.route("/menu")
+def menu():
+     return """
 <!doctype html>
 <html>
     <head>
-        <title>Каменева Наталья Анатольевна, лабораторная 1</title>
+        <title> НГТУ, ФБ, Лабораторные работы </title>
     </head>
     <body>
-        <header>
-            НГТУ, ФБ, Лабораторная работа 1
-        </header>
+    <header>
+        НГТУ, ФБ, WEB- программирование, часть 2. Список лабораторных, меню 
+    </header>
+    <ol> 
+        <li> 
+            <a href="/lab1" target="_blank"> Лабораторная работа 1 </a> 
+        </li> 
+    </ol> 
 
         <h1>web-сервер на flask</h1>
+        
+        <a href="/lab1">Первая лабораторная работа</a>
 
         <footer>
             &copy; Наталья Каменева, ФБИ-13, 3 курс, 2023
@@ -37,42 +47,33 @@ def lab1():
         </header>
         
         <h1>web-сервер на flask</h1>
+        <p>
             Flask — фреймворк для создания веб-приложений на языке
             программирования Python, использующий набор инструментов
             Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
             называемых микрофреймворков — минималистичных каркасов
             веб-приложений, сознательно предоставляющих лишь самые базовые возможности 
-         <ol>
-                <li>
-                    <a href="/menu" target="_blank"   > Меню </a>
-                </li>
-        </ol>
+        </p>
+            <a href="/menu" target="_blank"> Меню </a>
+        <ol>
         <h2>Реализованные роуты</h2>
+        </ol>
         <ol>
                 <li>
-                    <a href="/lab1/oak" target="_blank"> Картинка дуба</a>
+                    <a href="/lab1/oak" target="_blank">Дуб</a>
                 </li>
                 <li>
-                    <a href="/lab1/student" target="_blank"> ФИО студента НГТУ </a>
+                    <a href="/lab1/student" target="_blank">ФИО студента НГТУ</a>
                 </li>
                 <li>
-                    <a href="/lab1/python" target="_blank"> Информация про python </a>
+                    <a href="/lab1/python" target="_blank">Язык программирования Python</a>
                 </li>
                 <li>
-                    <a href="/lab1/color" target="_blank"> Произвольная страница </a>
+                    <a href="/lab1/cat" target="_blank">Кошки в Древнем Египте</a>
                 </li>
         </ol>
         <footer>
             &copy; Наталья Каменева, ФБИ-13, 3 курс, 2023
-        </footer>
-
-    </body>
-<html>
-    <header>
-        НГТУ, ФБ, WEB- программирование, часть 2. 
-    </header>
-<footer>
-            &copy; Каменева Наталья Анатольевна, ФБИ-11, 3 курс, 2023
         </footer>
     </body>
 </html>
@@ -80,71 +81,65 @@ def lab1():
 @app.route('/lab1/oak')
 def oak():
     return """
-<!doctype html>
-<html>
-"""
-@app.route('/lab1/student')
-def nstu():
-    return """
-<!doctype html>
-<html>
+    <!doctype html>
     <header>
-            НГТУ, ФБ, WEB- программирование, часть 2. 
+        НГТУ, ФБ, WEB- программирование, часть 2. 
     </header>
-    <body>
-     <body>
+    <html>
+        <body>
         <h1>Дуб</h1>
         <img src="''' + url_for('static', filename='oak.jpg') + '''">
         <link rel="stylesheet" href="''' +url_for('static', filename='lab1.css')+'''">
             <footer>
             &copy; Каменева Наталья Анатольевна, ФБИ-11, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-"""
+            </footer>
+        </body>
+    </html>
+    """
 @app.route('/lab1/student')
-def nstu1():
+def nstu():
     return """
-<!doctype html>
-<html>
+    <!doctype html>
     <header>
-            НГТУ, ФБ, WEB- программирование, часть 2. 
+        НГТУ, ФБ, WEB- программирование, часть 2. 
     </header>
-    <body>
-        <h1>Каменева Наталья Анатольевна</h1>
-        <img src="''' + url_for('static', filename='nstu.jpeg') + '''">
-        <link rel="stylesheet" href="''' +url_for('static', filename='lab1.css')+'''"
-        <footer> 
-        &copy; Каменева Наталья Анатольевна, ФБИ-11, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-"""
+    <html>
+        <body>
+            <h1>Каменева Наталья Анатольевна</h1>
+            <img src="''' + url_for('static', filename='nstu.jpeg') + '''">
+            <link rel="stylesheet" href="''' +url_for('static', filename='lab1.css')+'''"
+            <footer> 
+                &copy; Каменева Наталья Анатольевна, ФБИ-11, 3 курс, 2023
+            </footer>
+        </body>
+    </html>
+    """
 @app.route('/lab1/python')
 def python():
     return """
-<!doctype html>
-<html>
+    <!doctype html>
+    <html>
     <header>
-            НГТУ, ФБ, WEB- программирование, часть 2. 
+        НГТУ, ФБ, WEB- программирование, часть 2. 
     </header>
-    <body>
-        <h1>Python — это язык программирования, который широко используется в интернет-приложениях, разработке
-         программного обеспечения, науке о данных и машинном обучении (ML). Разработчики используют Python,
-          потому что он эффективен, прост в изучении и работает на разных платформах. Программы на языке 
-          Python можно скачать бесплатно, они совместимы
-<img src="''' + url_for('static', filename='python.jpg') + '''">
+        <body>
+        <p>
+        Python — это язык программирования, который широко используется в интернет-приложениях, разработке
+        программного обеспечения, науке о данных и машинном обучении (ML). Разработчики используют Python,
+        потому что он эффективен, прост в изучении и работает на разных платформах. Программы на языке 
+        Python можно скачать бесплатно, они совместимы
+        </p>
+        <img src="''' + url_for('static', filename='python.jpg') + '''">
 
         <link rel="stylesheet" href="''' +url_for('static', filename='lab1.css')+'''">
-        
-    <footer>
+        <footer>
             &copy; Каменева Наталья Анатольевна, ФБИ-13, 3 курс, 2023
-    </footer>
+        </footer>
     </body>
 </html>
 """
-@app.route('/lab1/color')
-def color():
+@app.route('/lab1/cat')
+def cat():
     return """
 <!doctype html>
 <html>
@@ -152,15 +147,16 @@ def color():
             НГТУ, ФБ, WEB- программирование, часть 2. 
     </header>
     <body>
-        <h1> Воздействие цветов на мозг</h1> <br>
-        <p> <b>Черный.</b> Одежда черного цвета стройнит людей, а мозгу помогает принимать более эффективные решения. <br>
-            <b>Желтый.</b> Он оказывает самое сильное воздействие на мозг, повышая вашу самооценку и креативность. <br>
-            <b>Синий/голубой.</b> Цвет спокойствия, помогает снять стресс и агрессию. В японском городе Нара на одной из опасных улиц города установили синие фонари, после чего количество преступлений снизилось там на 9% <br>
-            <b>Зеленый.</b> Снижает усталость, раздражение и физическую боль.Обратите внимание на то, что в больницах часто используют именно голубые и зеленые халаты.<br>
-            Покрасьте комнату в <b>фиолетовый</b>  и в ней станет прохладнее. В <b>оранжевый</b> - станет теплее. 
-            Эти цвета помогают мозгу сильнее воспринимать температуру.</p> 
-        <img src="''' + url_for('static', filename='color.jpg') + '''">    
-  <footer>
+        <h1>Кошки в Древнем Египте</h1> <br>
+        <p> Кошки в Древнем Египте достигли высокого статуса. 
+        Они охраняли жизненно важные посевы от грызунов и стали объектом восхищения.
+        Однако мурлыки не просто поедали мышей и крыс. Они также играли определённую роль в загробной жизни человека.
+        Древние египтяне носили амулеты с кошками, чтобы призвать защиту и благословение Бастет. Бесчисленные скульптуры 
+        кошек были сделаны в её честь и преподнесены в качестве жертвоприношений.</p> 
+        <img src="''' + url_for('static', filename='cat.jpg') + '''">    
+        <footer>
             &copy; Каменева Натальпя Анатольевна, ФБИ-11, 3 курс, 2023
         </footer> 
+    </body>
+</html>
 """
