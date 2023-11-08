18d7f9c0f40615ce19f4084d3205437cd52c155e
Каменева Наталья <kameneva.natalka@gmail.com>
2023-11-08 14:49:44 +0700

    Книги

--- app.py
+++ app.py
@@ -47,7 +47,19 @@ def example():
         {'name':'мандарины', 'price': 95},
         {'name':'манго', 'price': 321},
         ]
-    return render_template('example.html', name=name, group=group, course=course, lab_name=lab_name, fruits=fruits)  
+    books=[
+        {'booktitle' : 'Ночной портье', 'author' : 'Ирвин Шоу', 'genre' : 'Роман', 'numpages' : 352},
+        {'booktitle' : 'Преступление и наказание', 'author' : 'Федор Достоевский', 'genre' : 'Роман', 'numpages' : 608},    
+        {'booktitle' : 'Война и мир. Том 2', 'author' : 'Лев Толстой', 'genre' : 'Роман', 'numpages' : 416},   
+        {'booktitle' : 'Маленький принц', 'author' : ' Антуан де Сент-Экзюпери', 'genre' : 'Сказка', 'numpages' : 128}, 
+        {'booktitle' : 'Евгений Онегин',  'author' : 'Александр Пушкин', 'genre'  : 'Роман',  'numpages' : '456'},  
+        {'booktitle' : 'Дневник памяти', 'author' : 'Николас Спаркс', 'genre' : 'Любовный роман', 'numpages' : 256},
+        {'booktitle' : '451 градус по Фаренгейту', 'author' : 'Рей Бредбери', 'genre' : 'Роман-антиутопия', 'numpages' : 288},
+        {'booktitle' : 'Мастер и Маргарита', 'author' : 'Михаил Булгаков', 'genre' : 'Любовный роман', 'numpages' : 480},
+        {'booktitle' : 'Цветы для Элджернона', 'author' : 'Даниел Киз', 'genre' : 'Научная фантастика', 'numpages' : 384},
+        {'booktitle' : 'Герой нашего времени', 'author' : 'Михаил Лермонтов', 'genre' : 'Роман', 'numpages' : 288}, 
+        ]
+    return render_template('example.html', name=name, group=group, course=course, lab_name=lab_name, fruits=fruits, books=books)  
 
 @app.route("/lab1")
 def lab1():
