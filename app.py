c22c6f1c7afb7335e518e7ead7993287fbc6d014
Каменева Наталья <kameneva.natalka@gmail.com>
2023-11-22 13:22:55 +0700

    Правки меню

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, redirect, url_for, render_template
+from lab1 import lab1
+from lab2 import lab2
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -67,8 +70,8 @@ def lab2():
 
 @app.route ('/lab2/topphoto')
 def topphoto():
-     return render_template('topphoto.html')
-
+    return render_template('topphoto.html')
+app.register_blueprint(lab2)
 
 
 @app.route("/lab1")
