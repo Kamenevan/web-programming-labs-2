d8edb4b8322728faf1337df9dd6fb9efe4af3aac
Каменева Наталья <kameneva.natalka@gmail.com>
2023-12-06 13:00:36 +0700

    Лабораторная 5 изменения

--- app.py
+++ app.py
@@ -3,9 +3,28 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
+
+from flask_sqlalchemy import SQLAlchemy
+from Db import db
+from Db.models import users
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
+
+app.secret_key = '123'
+user_db = 'natasha_knowledge_base_orm'
+host_ip = '127.0.0.1'
+host_port = '5432'
+database_name = 'knowledge_base_orm'
+password = '12345'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
