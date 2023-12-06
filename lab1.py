54137051906670d4da16cce9e33ca76071a1cb92
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-12-06 18:23:30 +0700

    Изменения

--- lab1.py
+++ lab1.py
@@ -31,7 +31,15 @@ def menu():
             <li> 
                 <a href="/lab3/" target="_blank"> Лабораторная работа 3 </a> 
             </li> 
+            <li> 
+                <a href="/lab4/" target="_blank"> Лабораторная работа 4 </a> 
+            </li> 
+            <li> 
+                <a href="/lab5/" target="_blank"> Лабораторная работа 5 </a> 
+            </li> 
         </ol> 
+
+
         
         <footer>
             &copy; Наталья Каменева, ФБИ-13, 3 курс, 2023
