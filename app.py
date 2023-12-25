8e5fedbaf5bf18cfba597fb278a8f96003742409
Каменева Наталья <kameneva.natalka@gmail.com>
2023-12-26 02:10:58 +0700

    Лабораторная 9

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
+from lab8 import lab8
+from lab9 import lab9
 
 from flask_sqlalchemy import SQLAlchemy
 from Db import db
@@ -39,3 +42,6 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
+app.register_blueprint(lab8)
+app.register_blueprint(lab9)
